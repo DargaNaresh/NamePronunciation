92fbf9e67a54d7e51a4ab35cb0eb78ffd573d20c
chinnisrikanth <chinniharsha@outlook.com>
2022-05-13 18:18:15 +0530

    Login and Search pages

---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserModel model)
+        {
+            Users oUser = new Users();
+            var item = oUser.CheckUser(model.UserName, model.Password);
+            
+            if (item != null && item.Count<UserModel>() > 0)
+            {
+                //return View("PronounceName");
+                //RedirectToAction("Index", "PronounceName",);
+                return RedirectToActionPermanent("Index", "PronounceName");
+            }
+            else if (item == null)
+            {
+                ViewBag.NotValidUser = item;
+            }
+            else
+            {
+                ViewBag.Failedcount = item;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebApp/Controllers/PronounceNameController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class PronounceNameController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            SearchUserModelList lst = new SearchUserModelList();
+            lst.SearchUserData = GetUsers();
+            return View(lst);
+        }
+
+        [HttpPost]
+        public IActionResult SearchUser([FromBody] string qry)
+        {
+            SearchUserModelList lst = new SearchUserModelList();
+            List<SearchUserModel> lstSUM= GetUsers();
+            lst.SearchUserData = lstSUM.Where(a => a.Name.Contains(qry) || a.LanId.Contains(qry) || a.Email.Contains(qry) || a.EmpNum.Contains(qry)).ToList();
+            
+            return View(lst);
+        }
+
+        public List<SearchUserModel> GetUsers()
+        {
+            List<SearchUserModel> lstSUM = new List<SearchUserModel>();
+            SearchUserModel sm = new SearchUserModel();
+            sm.Name = "Srikanth";
+            sm.EmpNum = "1234";
+            sm.Email = "srikanth@test.com";
+            sm.LanId = "u567157";
+            lstSUM.Add(sm);
+
+            sm = new SearchUserModel();
+            sm.Name = "Naresh";
+            sm.EmpNum = "1235";
+            sm.Email = "naresh@test.com";
+            sm.LanId = "u123456";
+            lstSUM.Add(sm);
+
+            sm = new SearchUserModel();
+            sm.Name = "Sailaja";
+            sm.EmpNum = "1235";
+            sm.Email = "sailaja@test.com";
+            sm.LanId = "u123456";
+            lstSUM.Add(sm);
+
+            return lstSUM;
+        }
+    }
+}
