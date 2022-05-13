f48ed876437724e7044713cf128520a76742d272
NareshDarga <nareshdarga@gmail.com>
2022-05-13 21:35:53 +0530

    added logic for download and upload

--- WebAPI/Controllers/NameController.cs
+++ WebAPI/Controllers/NameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Speech.Synthesis;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -15,22 +16,22 @@ namespace WebAPI.Controllers
     [ApiController]
     public class NameController : ControllerBase
     {
-        private IHostingEnvironment Environment;
 
-        public NameController(IHostingEnvironment _environment)
+        public NameController()
         {
-            Environment = _environment;
-            //_logger = logger;
+
         }
         public static byte[] content;
+        public static string userlanID;
 
 
         [HttpPost, DisableRequestSizeLimit]
-        public ActionResult Upload(IFormFile audio, [FromForm] string email, [FromForm] string lanID, [FromForm] string employeeID)
+        public ActionResult Upload(IFormFile audio, [FromForm] string email,[FromForm] string lanID, [FromForm] string employeeID)
         {
             //web app will record the mp3 and will send the mp3 to this api
             if (ModelState.IsValid)
             {
+                userlanID = lanID;
                 if (audio != null && audio.Length > 0)
                 {
                     var reader = new System.IO.BinaryReader(audio.OpenReadStream());
@@ -47,38 +48,52 @@ namespace WebAPI.Controllers
         [Route("text")]
         public ActionResult Upload([FromForm] string text)
         {
-                string contentPath = this.Environment.ContentRootPath;
+            //string contentPath = this.Environment.ContentRootPath;
 
-                string path = Path.Combine(this.Environment.ContentRootPath, "Uploads");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                string fullpath = string.Format("{0}\\{1}", path, "Test.wav");
+            //string path = Path.Combine(this.Environment.ContentRootPath,             "Uploads");
+            //if (!Directory.Exists(path))
+            //{
+            //    Directory.CreateDirectory(path);
+            //}
+            //string fullpath = string.Format("{0}\\{1}", path, "Test.wav");
 
-                byte[] data;
+            //byte[] data;
 
-                using (var fs = new FileStream(fullpath, FileMode.Create, FileAccess.Write))
-                {
-                    SpeechSynthesizer speech = new SpeechSynthesizer();
-                    speech.Speak(text);
-                   // int length = Convert.ToInt32(fs.Length);
-                   // data = new byte[length];
-                //    fs.Read(data, 0, length);
-                  //  speech.SetOutputToWaveStream(fs);
-                };
+            //using (var fs = new FileStream(fullpath, FileMode.Create, 
+
+            //FileAccess.Write))
+            //{
+            SpeechSynthesizer speech = new SpeechSynthesizer();
+            speech.Speak(text);
+            // int length = Convert.ToInt32(fs.Length);
+            // data = new byte[length];
+            //    fs.Read(data, 0, length);
+            //  speech.SetOutputToWaveStream(fs);
             return Ok("successful");
 
             //return File(data, "audio/vnd.wav");
         }
 
 
+        // Download MP3 Driectly : Naresh - Dnt delete
+        //[HttpGet, DisableRequestSizeLimit]
+        //public async Task<IActionResult> Download()
+        //{
+        //    //await
+        //    return File(content, "audio/mpeg", userlanID + ".mp3");
+        //}
 
         [HttpGet, DisableRequestSizeLimit]
-    public async Task<IActionResult> Download()
-    {
-        //await
-        return File(content, "audio/mpeg");
+        public byte[] Download()
+        {
+            byte[] mybytearray = content;
+            return mybytearray;
+        }
+
+
     }
-}
+
+
+
+
 }
--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApp.Models;
@@ -12,6 +18,7 @@ namespace WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,15 +30,70 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        //upload
+        [HttpPost]
+        public IActionResult Index(IFormFile postedFile)
         {
+
+            byte[] data;
+            MultipartFormDataContent multiForm = new MultipartFormDataContent();
+            multiForm.Add(new StringContent("narehdarga@gmail.com"), "email");
+            multiForm.Add(new StringContent("1473635"), "employeeID");
+            multiForm.Add(new StringContent("u164207"), "lanID");
+
+            if (postedFile.Length > 0)
+            {
+                ByteArrayContent bytes;
+                using (var br = new BinaryReader(postedFile.OpenReadStream()))
+                {
+                    data = br.ReadBytes((int)postedFile.Length);
+                }
+                bytes = new ByteArrayContent(data);
+                multiForm.Add(bytes, "audio", postedFile.FileName);
+            }
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage httpResponseMessage = client.PostAsync("http://localhost:50015/api/name", multiForm).Result;
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                var content = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            }
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //download
+        public IActionResult Privacy()
+        {
+            string apiUrl = "http://localhost:50015/api/name";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = new HttpResponseMessage();
+            byte[] mybytearray = null;
+            response = client.GetAsync(apiUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = null;
+                result = response.Content.ReadAsStringAsync().Result.Replace("\"",string.Empty);
+                mybytearray = Convert.FromBase64String(result);
+            }
+            ViewBag.Data = "data:audio/wav;base64," + Convert.ToBase64String(mybytearray);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore= true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ??HttpContext.TraceIdentifier
+            });
         }
+
+
     }
 }
